a71347a0986101ac25bb801ee940086da6e48f36
BEN-PC\Ben <benji.lam@hotmail.ca>
2017-12-16 14:01:35 -0500

    Initial commit

---
+++ BudgetMoiCa/BudgetMoiCa/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BudgetMoiCa.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
